86dd2c0ed9f76812c244aae989d4a91d97439f31
ginohmk <michotall95@gmail.com>
2023-12-22 09:13:28 +0100

    feat: Folder structure

---
+++ src/pages/home/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from './NavBar';
+import Banner from './Banner';
+import MainSection from './MainSection';
+import Footer from './Footer';
+
+const HomePage = () => {
+  return (
+    <>
+      <NavBar />
+      <Banner />
+      <MainSection />
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
